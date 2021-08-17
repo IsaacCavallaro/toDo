f5d343cdd78f6ba3bf836153569a5ddf52bbf0fa
isaaccavallaro <isaaccavallaro@gmail.com>
2021-08-17 13:54:29 +1000

    adding css animation for deleting items

--- app.js
+++ app.js
@@ -57,7 +57,12 @@ function deleteCheck(e){
     //Delete todo item
     if(item.classList[0] === 'trash-btn'){
         const todo = item.parentElement;
-        todo.remove(); 
+        //animation
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function(){
+            todo.remove()
+        });
+        
     }
 
     //check mark
